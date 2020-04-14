f8ceb342dd1649a7be176e77f2a1d5f7a8f11e7f
William Mittard <billymittard@ymail.com>
2020-04-14 16:38:45 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,8 +16,11 @@ def speak_to_grandma(greeting)
   if greeting.upcase == "I LOVE YOU GRANDMA!"
     'I LOVE YOU TOO PUMPKIN!'
   elsif greeting.upcase == greeting
-    "NO NOT SINCE 1938" 
+    "NO NOT SINCE 1938!" 
   else 
-   ""
+   "HUH?! SPEAK UP, SONNY!"
+ end 
+ 
+ end
   
   
